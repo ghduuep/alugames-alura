e550fcafc17a192af2946c18d1bea45253d84fae
Guilherme Lima <ghduep@outlook.com>
2024-02-15 14:19:49 -0300

    adicionar contador de jogos alugados

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+let jogosAlugados = 0;
+
 function alterarStatus(numero_do_jogo) {
+    let jogoNome = document.querySelector(`#game-${numero_do_jogo} .dashboard__item__name`);
     let jogoImagem = document.querySelector(`#game-${numero_do_jogo} .dashboard__item__img`);
     if (jogoImagem.classList.contains('dashboard__item__img--rented')) {
         jogoImagem.classList.remove('dashboard__item__img--rented');
@@ -7,18 +10,31 @@ function alterarStatus(numero_do_jogo) {
     }
 
     let jogoTexto = document.querySelector(`#game-${numero_do_jogo} .dashboard__item__button`);
-    let jogoNome = document.querySelector(`#game-${numero_do_jogo} .dashboard__item__name`);
     if (jogoTexto.classList.contains('dashboard__item__button--return')) {
         let confirmacaoUsuario = prompt(`Tem certeza que deseja devolver ${jogoNome.textContent}? S/N`).toLowerCase()
         if (confirmacaoUsuario == 's') {
             jogoTexto.classList.remove('dashboard__item__button--return');
             jogoTexto.innerHTML = 'Alugar';
+            jogosAlugados--;
+
         } else {
             return;
         }
     } else {
         jogoTexto.classList.add('dashboard__item__button--return');
-        jogoTexto.innerHTML = 'Devolver'
+        jogoTexto.innerHTML = 'Devolver';
+        jogosAlugados++;
 
     }
+
+    contarEExibirJogosAlugados()
 }
+
+function contarEExibirJogosAlugados() {
+    console.log(`Você alugou ${jogosAlugados}`);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+})
